3c7a0e16078e0e076670879733af726af878b670
Nandan kumar <kumarnandan551@gmail.com>
2024-12-28 18:54:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-function firstWord(s) {
-  // your code here
+function firstWord(str) {
+  // Trim leading and trailing spaces to handle cases where spaces are at the beginning or end
+  str = str.trim();
+
+  // Find the index of the first space
+  const spaceIndex = str.indexOf(' ');
+
+  // If no space is found, return the entire string
+  if (spaceIndex === -1) {
+    return str;
+  }
+
+  // Otherwise, return the substring from the beginning to the first space
+  return str.substring(0, spaceIndex);
 }
 
-// Do not change the code below
-
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// Examples
+console.log(firstWord('see and stop')); // Output: 'see'
+console.log(firstWord(' Hello World!')); // Output: 'Hello'
+console.log(firstWord('12345')); // Output: '12345'
+console.log(firstWord('')); // Output:
